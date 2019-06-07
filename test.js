999d921ededd6d3af6e8656c8eb7d930b33bc4d1
ChaeYeon Park <yeoni1098@naver.com>
2019-06-08 00:23:39 +0900

    그래프의 모양 변경, 영어 변역, 글자크기 변경, margin 추가

--- test.js
+++ test.js
@@ -58,7 +58,7 @@ export default class WeatherDetailScreen extends React.Component {
         if (this.state.isLoading) {
             return (
                 <View style={styles.container}>
-                    <Text>데이터를 불러오는 중입니다.</Text>
+                    <Text style={{textAlign:"center"}}>데이터를 불러오는 중입니다.</Text>
                 </View>
             )
         }
@@ -78,13 +78,13 @@ export default class WeatherDetailScreen extends React.Component {
             summary = <ScrollView
                 style={{flex:0.2}}>
                 <Text
-                    style={{textAlign:"center"}}>{this.state.query.pages[hello].extract}</Text>
+                    style={{textAlign:"center",color:'#fafafa'}}>{this.state.query.pages[hello].extract}</Text>
             </ScrollView>;
         } else {
             summary = <ScrollView
-                style={{flex:0.2, backgroundColor: 'rgba(242, 241, 239, 1)'
+                style={{flex:0.2, backgroundColor: '#fafafa'
                 }}
-            ><Text style={{textAlign:"center",fontSize:20}}>"Pin을 클릭하면 지역 정보를 얻을 수 있습니다."</Text></ScrollView>;
+            ><Text style={{textAlign:"center",fontSize:15,color:'rgb(2,7,21)',marginTop: "5%"}}>Pin을 클릭하면 지역 정보를 얻을 수 있습니다.</Text></ScrollView>;
         }
 
         return (
@@ -92,49 +92,42 @@ export default class WeatherDetailScreen extends React.Component {
             <View style={styles.container}>
 
                 <AreaChart
-                    style={{flex:0.2}}
+                    style={{flex:0.4,marginTop:"5%"}}
                     data={ new_data }
                     // contentInset={{ top: 30, bottom: 30 }}
-                    curve={ shape.curveNatural }
-                    svg={{ fill: 'rgba(255, 0, 0, 0.2)' }}
+                    curve={ shape.curveBasis}
+                    svg={{ fill: 'rgba(0, 0, 0, 0.2)'}}
                 >
                 </AreaChart>
 
                 <Slider
-                    style={{backgroundColor:'rgba(242, 241, 239, 1)',flex:0.05}}
+                    style={{backgroundColor:'#fafafa',flex:0.1}}
                     value={this.state.value}
                     onValueChange={value => this.setState({ value } )}
                 />
                 <View
-                    style = {{backgroundColor:'rgba(255, 0, 0, 0.2)', alignItems:'center'}}>
+                    style = {{backgroundColor:'#fafafa', alignItems:'center', margin:"5%"}}>
                     <Text style={{
-                        color:'black',
-                        fontSize:20,
-                        textShadowColor: 'white',
-                        textShadowOffset: { width: 1, height: 1 },
-                        textShadowRadius: 1,
+                        color:'rgb(2,7,21)',
+                        fontSize:19,
+
                     }}>{get_temp.list[parseInt(this.state.value * 35)]["dt_txt"]}</Text>
+                    <Text/>
                     <Text style={{
-                        color:'black',
-                        fontSize:20,
-                        textShadowColor: 'white',
-                        textShadowOffset: { width: 1, height: 1 },
-                        textShadowRadius: 1,
-                    }}>Temperature : {(get_temp.list[parseInt(this.state.value * 35)]["main"]["temp"] - 273.15).toFixed(1)}</Text>
+                        color:'rgb(2,7,21)',
+                        fontSize:17,
+                    }}>온도 {(get_temp.list[parseInt(this.state.value * 35)]["main"]["temp"] - 273.15).toFixed(1)}도</Text>
                     <Text style={{
-                        color:'black',
-                        fontSize:20,
-                        textShadowColor: 'white',
-                        textShadowOffset: { width: 1, height: 1 },
-                        textShadowRadius: 1,
-                    }}>Humidity : {get_temp.list[parseInt(this.state.value * 35)]["main"]["humidity"]}</Text>
+                        color:'rgb(2,7,21)',
+                        fontSize:17,
+
+                    }}>습도 {get_temp.list[parseInt(this.state.value * 35)]["main"]["humidity"]}</Text>
                     <Text style={{
-                        color:'black',
-                        fontSize:20,
-                        textShadowColor: 'white',
-                        textShadowOffset: { width: 1, height: 1 },
-                        textShadowRadius: 1,
-                    }}>{get_temp.list[parseInt(this.state.value * 35)].weather[0]["main"]}</Text>
+                        color:'rgb(2,7,21)',
+                        fontSize:17,
+
+
+                    }}>현재 날씨 {get_temp.list[parseInt(this.state.value * 35)].weather[0]["main"]}</Text>
                 </View>
                 {/*<Text*/}
                 {/*style={{backgroundColor: "gray"}}>{celsius}</Text>*/}
@@ -183,10 +176,11 @@ export default class WeatherDetailScreen extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor:'#fafafa',
         flex: 1,
         // marginTop: Constants.statusBarHeight,
     }, hello : {
-        height:"70%"
+        height:"50%"
 
     }
 });
